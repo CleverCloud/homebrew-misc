962640c8fafeb95a0398e414eb3dba5ef71a54cd
Clever Cloud CI <opensource@clever-cloud.com>
2026-09-06 23:04:46 +0000

    Update mdr to 0.4.0

--- Formula/mdr.rb
+++ Formula/mdr.rb
@@ -1,22 +1,27 @@
 class Mdr < Formula
   desc "A lightweight Markdown viewer with Mermaid diagram support"
   homepage "https://github.com/CleverCloud/mdr"
-  version "0.3.2"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/CleverCloud/mdr/releases/download/v0.3.2/mdr-aarch64-apple-darwin.tar.gz"
-      sha256 "e0838c37de1c8e25aef6bf7b5aa95acf4f3970f4f722c612139e1e8c7028ac4c"
+      url "https://github.com/CleverCloud/mdr/releases/download/v0.4.0/mdr-aarch64-apple-darwin.tar.gz"
+      sha256 "188c5890f50f6a45b8fc9540f256a1677a5df1cbe0f7b8817268555f2ab0f6c2"
     else
-      url "https://github.com/CleverCloud/mdr/releases/download/v0.3.2/mdr-x86_64-apple-darwin.tar.gz"
-      sha256 "660b0a8facbe2463a1f99cc85701dc125372a37ece9a591a6f729108f77e949e"
+      url "https://github.com/CleverCloud/mdr/releases/download/v0.4.0/mdr-x86_64-apple-darwin.tar.gz"
+      sha256 "38c9ad5a375b55a069d51757798244a81b4289f628e9bfcdd197dbb85f77bb7b"
     end
   end
 
   on_linux do
-    url "https://github.com/CleverCloud/mdr/releases/download/v0.3.2/mdr-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "7e04435e8174b6babd437f520fd672d83e732f6deb7fc10aabfb0f3f991101b2"
+    if Hardware::CPU.arm?
+      url "https://github.com/CleverCloud/mdr/releases/download/v0.4.0/mdr-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "7eb670f10d4b2fd0277a7d81cc197e3f1b93890ccc8b2ddcc64da5fd4407e1db"
+    else
+      url "https://github.com/CleverCloud/mdr/releases/download/v0.4.0/mdr-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "4a6f7680be802c3759c2d1f4a50b27c872e52913833f6e8c4b15c4b9513512a8"
+    end
   end
 
   def install
